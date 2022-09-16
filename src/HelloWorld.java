8c44f9df1240e3dabb3b23854be1401365de68e4
dxsattler <daniel.sattler2@igt.com>
2022-09-16 19:05:00 +0200

    Hello World Erste Vorlesung Konfigurationsmanagement

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    // main + TAB
+    // psvm + TAB
+    public static void main(String[] args) {
+        // sout + TAB
+        System.out.println("Hello World!");
+        System.out.println("Welcome to KNOF 2022");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); //STRG + D - fügt die obere Zeile neu ein
+
+    }
+}
